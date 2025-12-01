f173768800136c2d4625eec2c699c27e132176b5
Soobin <mhoang26ct@gmail.com>
2025-12-01 23:16:04 +0700

    Revert "Chore/Refactor: Project Structure Alignment and VS Upgrade"
    This reverts commit 719e13dfc13c452e022c0a3eff1f0d1a51a0e5ff.

--- ProjectRestaurant/Properties/Settings.Designer.cs
+++ ProjectRestaurant/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RestaurantManagement.Properties {
+namespace ProjectRestaurant.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
