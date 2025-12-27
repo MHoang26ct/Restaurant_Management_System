c910338a2e9f0b331c4e5f41d67bd2ce0a444e5c
Soobin <mhoang26ct@gmail.com>
2025-12-28 01:28:16 +0700

    Feat: Add DeleteOrderByReservation stored procedure

--- Database/StoredProcedure.sql
+++ Database/StoredProcedure.sql
@@ -508,6 +508,35 @@ BEGIN
     WHERE OrderID = @OrderID
 END
 
+-- Xóa order kèm theo chi tiết order bằng ReservationID
+GO
+CREATE PROCEDURE DeleteOrderByReservation
+    @ReservationID int
+AS
+BEGIN
+    BEGIN TRANSACTION;
+
+    BEGIN TRY
+        DELETE FROM OrderDetails
+        WHERE OrderID IN (
+            SELECT OrderID 
+            FROM Orders 
+            WHERE ReservationID = @ReservationID
+        );
+
+        DELETE FROM Orders
+        WHERE ReservationID = @ReservationID;
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR(@ErrorMessage, 16, 1);
+    END CATCH
+END
+GO
+
 -- =============================================
 -- NHÓM 6: QUẢN LÝ NHÂN VIÊN (EMPLOYEES)
 -- =============================================
